4cd3edee679f641c537afba67f6a52537a34d857
Held Grijo <held.grijo@gmail.com>
2023-04-18 16:04:17 -0400

    feat: add stylized buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { useState } from 'react'
+import { Button } from "./components/Button";
 
 export function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1>Ignite Timer</h1>
+    <>
+      <Button color="primary"/>
+      <Button color="secondary"/>
+      <Button color="success"/>
+      <Button color="danger"/>
+      <Button />
+    </>
   )
 }
