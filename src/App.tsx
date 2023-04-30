280e98735080d4d59342560aaffbfdc314f89df3
Held Grijo <held.grijo@gmail.com>
2023-04-30 19:14:46 -0400

    fix: add files fixed by ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global";
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
@@ -13,5 +13,5 @@ export function App() {
       <Button />
       <GlobalStyle />
     </ThemeProvider>
-  );
+  )
 }
