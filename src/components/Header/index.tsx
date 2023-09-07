a18d8175be5a4f1831edbc6adac416ccd3076659
Held Grijo <held.grijo@gmail.com>
2023-09-07 16:25:48 -0400

    feat: add icons to the header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 import { HeaderContainer } from './styles'
 
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { Scroll, Timer } from 'phosphor-react'
+
 export function Header() {
   return (
     <HeaderContainer>
-      <span>logo</span>
+      <img src={logoIgnite} alt="" />
       <nav>
-        <a href="">timer</a>
-        <a href="">history</a>
+        <a href="">
+          <Timer size={24} />
+        </a>
+        <a href="">
+          <Scroll size={24} />
+        </a>
       </nav>
     </HeaderContainer>
   )
