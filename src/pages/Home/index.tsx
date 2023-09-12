1b7682ddb52a6d923601386f97bdd5773abb5d56
Held Grijo <held.grijo@gmail.com>
2023-09-11 22:40:35 -0400

    feat: add datalist suggestions for task input

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -16,7 +16,17 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome para seu projeto" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para seu projeto"
+          />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
